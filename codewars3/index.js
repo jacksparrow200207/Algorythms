2d7189c57dcf6171285f688c1e451824a8430d63
jacksparrow200207 <zuhriddin02b@gmail.com>
2023-05-11 23:39:45 +0500

    algorythm

---
+++ codewars3/index.js
@@ -0,0 +1,42 @@
+// Task 10: write me a function stringy that takes a size and returns a string of alternating '1s' and '0s'.
+
+// the string should start with a 1.
+
+// a string with size 6 should return :'101010'.
+
+// with size 4 should return : '1010'.
+
+// with size 12 should return : '101010101010'.
+
+// The size will always be positive and will only use whole numbers.
+
+function stringy(size) {
+  return ''.padStart(size, '10')
+}
+
+
+
+// Task 11: Nathan loves cycling.
+
+// Because Nathan knows it is important to stay hydrated, he drinks 0.5 litres of water per hour of cycling.
+
+// You get given the time in hours and you need to return the number of litres Nathan will drink, rounded to the smallest value.
+
+// For example:
+
+// time = 3 ----> litres = 1
+
+// time = 6.7---> litres = 3
+
+// time = 11.8--> litres = 5
+
+function litres(time) {
+  let litres = 0.5
+  let result = 0
+  let timeInteger = Math.floor(time)
+  for(let i = 0; i < timeInteger; ++i) {
+    result += litres
+  }
+  
+  return Math.floor(result);
+}
