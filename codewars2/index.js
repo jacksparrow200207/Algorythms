8c1cf3bdf35ad3222ed4d442fbd9616b3163fd2b
jacksparrow200207 <zuhriddin02b@gmail.com>
2023-05-17 23:35:06 +0500

    algorythm

--- codewars2/index.js
+++ codewars2/index.js
@@ -1,40 +1,8 @@
-// Task 7: Given three integers a ,b ,c, return the largest number obtained after inserting the following operators and brackets: +, *, ()
-// In other words , try every combination of a,b,c with [*+()] , and return the Maximum Obtained (Read the notes for more detail about it)
-// Example
-// With the numbers are 1, 2 and 3 , here are some ways of placing signs and brackets:
+// console.log(parseInt('00010101', 2));
+// console.log(parseInt('00010011', 2));
 
-// 1 * (2 + 3) = 5
-// 1 * 2 * 3 = 6
-// 1 + 2 * 3 = 7
-// (1 + 2) * 3 = 9
-// So the maximum value that you can obtain is 9.
+// let num = 19 + 21;
+// let a = 1;
+// let b = 2;
+// console.log(a,b = b,a);
 
-// function expressionMatter(a, b, c) {
-//   let result = []
-//   result[0] = a * (b + c)
-//   result[1] = a * b * c
-//   result[2] = a + b * c
-//   result[3] = (a + b) * c
-//   result[4] = a + b + c
-  
-//   return Math.max(...result)
-// }
-
-
-// Task 8: Complete the function that takes two integers (a, b, where a < b) and return an array of all integers between the input parameters, including them.
-
-// For example:
-
-a = 1
-b = 4
-// --> [1, 2, 3, 4]
-
-function between(a, b) {
-  let results = [a]
-  let max = results.slice(-1)
-
-  for(let i = max; i < b; i++) {
-    results.push(a += 1)
-  }
-  return results
-}
---
+++ codewars4/index.js
@@ -0,0 +1,56 @@
+Task 11: Nathan loves cycling.
+
+// Because Nathan knows it is important to stay hydrated, he drinks 0.5 litres of water per hour of cycling.
+
+// You get given the time in hours and you need to return the number of litres Nathan will drink, rounded to the smallest value.
+
+// For example:
+
+// time = 3 ----> litres = 1
+
+// time = 6.7---> litres = 3
+
+// time = 11.8--> litres = 5
+
+function litres(time) {
+  let litres = 0.5
+  let result = 0
+  let timeInteger = Math.floor(time)
+  for(let i = 0; i < timeInteger; ++i) {
+    result += litres
+  }
+  
+  return Math.floor(result);
+}
+
+
+
+
+
+
+
+
+
+// Task 12: Given 2 strings, a and b, return a string of the form short+long+short, with the shorter string on the outside and the longer string on the inside. The strings will not be the same length, but they may be empty ( zero length ).
+
+// Hint for R users:
+
+// The length of string is not always the same as the number of characters
+// For example: (Input1, Input2) --> output
+
+// ("1", "22") --> "1221"
+// ("22", "1") --> "1221"
+
+// Answer
+
+function solution(a, b){
+  let aLength = a.length
+  let bLength = b.length
+  let result = ""
+  if(aLength > bLength) {
+    result = b + a + b 
+  } else {
+    result = a + b + a
+  }
+  return result
+}
