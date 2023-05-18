281e5ece984440b43405b7a1b8feffe0b7caa69e
jacksparrow200207 <zuhriddin02b@gmail.com>
2023-05-18 23:14:35 +0500

    algorythm

--- codewars5/index.js
+++ codewars5/index.js
@@ -24,13 +24,6 @@ function litres(time) {
 }
 
 
-
-
-
-
-
-
-
 // Task 12: Given 2 strings, a and b, return a string of the form short+long+short, with the shorter string on the outside and the longer string on the inside. The strings will not be the same length, but they may be empty ( zero length ).
 
 // Hint for R users:
@@ -41,7 +34,7 @@ function litres(time) {
 // ("1", "22") --> "1221"
 // ("22", "1") --> "1221"
 
-// Answer
+// // Answer
 
 function solution(a, b){
   let aLength = a.length
